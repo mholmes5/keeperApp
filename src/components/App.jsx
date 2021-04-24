04939ac2839f9fb9a97caa1e63ca96d3bfabfa32
Michael Holmes <mholmes5@asu.edu>
2021-04-24 18:07:27 -0500

    Setup part 3 and add uuid package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
 import notes from '../notes';
+import CreateArea from "./CreateArea";
+import { v4 as uuidv4 } from 'uuid';
 
 function App(){
     return (
